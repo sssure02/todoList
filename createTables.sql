ad29b6d0b3da2f271e8dfcfe7a61df19e08aaa88
Sai Sravani Sure <93887356+sssure02@users.noreply.github.com>
2023-03-26 16:15:07 -0400

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -19,3 +19,17 @@ task_status ENUM("active", "completed") NOT NULL,
 PRIMARY KEY(task_id),
 FOREIGN KEY(task_category) REFERENCES taskCategory(category_id)
 );
+
+/* Dummy Data */
+
+INSERT INTO taskCategory(category_name) VALUES ("Worksheet");
+INSERT INTO taskCategory(category_name) VALUES ("HW");
+INSERT INTO taskCategory(category_name) VALUES ("Assignment");
+INSERT INTO taskCategory(category_name) VALUES ("Quiz");
+
+INSERT INTO task (task_desp, due_date, task_category, priority_level, task_status)
+VALUES ('Complete Math Wk', '2023-03-30', 1, 3, 'active'),
+       ('Finish English Assignment', '2023-03-20', 3, 2, 'active'),
+       ('HW 3 for CSC 4710', '2023-03-25', 2, 1, 'completed'),
+       ('Finish quiz on Canvas', '2023-04-10', 4, 4, 'active'),
+       ('Finish econ wk', '2023-03-22', 1, 2, 'completed');
